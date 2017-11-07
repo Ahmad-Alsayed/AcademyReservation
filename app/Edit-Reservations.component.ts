8b9a54ba62c4cf20fbd8546baad2c68c57aee06b
Ahmad Alsayed <ahmadalsayed@Ahmads-MacBook-Pro.local>
2017-11-07 09:48:10 +0200

    added MyReservationService Class Constructor

--- app/Edit-Reservations.component.ts
+++ app/Edit-Reservations.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from 'angular2/core';
 import {MyReservationsService} from './MyReservations';
 
 const myres: MyReservationsService[] = [
-  {id:1,employeeId:1,date: new Date(),familyMembersAbove3Years:0,familyMembersUnder3Years:0,guestMembersAbove3Years:0,guestMembersUnder3Years:0},
-  {id:2,employeeId:1,date: new Date(),familyMembersAbove3Years:2,familyMembersUnder3Years:1,guestMembersAbove3Years:0,guestMembersUnder3Years:0},
-  {id:3,employeeId:2,date: new Date(),familyMembersAbove3Years:5,familyMembersUnder3Years:0,guestMembersAbove3Years:2,guestMembersUnder3Years:1},
-  {id:4,employeeId:3,date: new Date(),familyMembersAbove3Years:3,familyMembersUnder3Years:4,guestMembersAbove3Years:1,guestMembersUnder3Years:0}
-];
+  new MyReservationsService(1,1,new Date(),0,0,0,0),
+  new MyReservationsService(2,1,new Date(),2,1,0,0),
+  new MyReservationsService(3,2,new Date(),5,0,2,1),
+  new MyReservationsService(4,3,new Date(),3,4,1,0)
+
+  ];
 @Component({
   selector: 'Edit-Reservations',
   templateUrl: 'app/Edit-Reservations.component.html',
--- app/MyReservations.ts
+++ app/MyReservations.ts
@@ -3,7 +3,15 @@
 
 export class MyReservationsService {
 
-constructor() { }
+constructor(id: number,eid: number, date: Date, fa: number, fu: number, ga: number, gu: number, ) {
+    this.id = id;
+    this.employeeId = eid;
+    this.date = date;
+    this.familyMembersAbove3Years = fa;
+    this.familyMembersUnder3Years = fu;
+    this.guestMembersAbove3Years = ga;
+    this.guestMembersUnder3Years = gu;
+ }
 id: Number;
 familyMembersAbove3Years: Number;
 familyMembersUnder3Years: Number;
